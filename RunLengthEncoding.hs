259c6bc0d70a7c6bca4267af4a3582294f0d2cf2
ElectrixFox <lukecphillips2006@gmail.com>
2025-01-10 15:28:17 +0000

    RLE Encoding.
    Got the encoding working.

--- RunLengthEncoding.hs
+++ RunLengthEncoding.hs
@@ -3,23 +3,23 @@ import Data.List
 -- rleEncode
 getCount :: String -> (Integer, Char)
 getCount [] = (0, '.')
-getCount str = (fromIntegral (length str), (str) !! 0)
+getCount str = (fromIntegral (length str), str !! 0)
 
 getCounts :: [String] -> [(Integer, Char)]
 getCounts [] = [(0, '.')]
-getCounts s = map (getCount) s
+getCounts s = map getCount s
 
 -- rleEncode "aaaabbaaa" -> [(4,'a'), (2, 'b'), (3, 'a')]
 rleEncode :: String -> [(Integer, Char)]
--- rleEncode _ = [(0, 'a')]
 rleEncode inp = getCounts (group inp)
 -- rleEncode inp = [0, group (inp)]
 
 -- rleDecode [(4,'a'), (2, 'b'), (3, 'a')] -> "aaaabbaaa"
+rleDecode :: [(Integer, Char)] -> String
+rleDecode inp = replicate inp
 
 main :: IO ()
 main = do
-  print $ rleEncode "aaaabbaaa"
-  print $ group "aaaabbaaa"
-  print $ getCount ((group "aaaabbaaa") !! 0)
-  print $ getCounts (group "aaaabbaaa")
+  let code = "aaaabbaaa"
+  print $ rleEncode code
+  print $ rleDecode (rleEncode code)
