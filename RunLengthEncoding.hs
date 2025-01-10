794487b2ac8a3e5a8c877ea8f6697b44bbba86d9
ElectrixFox <lukecphillips2006@gmail.com>
2025-01-10 16:03:33 +0000

    RLE Completed
    I have completed the RLE algorithm.

--- RunLengthEncoding.hs
+++ RunLengthEncoding.hs
@@ -12,23 +12,17 @@ getCounts s = map getCount s
 -- rleEncode "aaaabbaaa" -> [(4,'a'), (2, 'b'), (3, 'a')]
 rleEncode :: String -> [(Integer, Char)]
 rleEncode inp = getCounts (group inp)
--- rleEncode inp = [0, group (inp)]
-
--- rleDecode
-decodeStep :: (Integer, Char) -> String
-decodeStep encoded = replicate (fromIntegral (fst encoded)) (snd encoded)
 
 -- rleDecode [(4,'a'), (2, 'b'), (3, 'a')] -> "aaaabbaaa"
 rleDecode :: [(Integer, Char)] -> String
 rleDecode [] = " " 
-rleDecode encoded = concat (map (decodeStep) encoded)
--- rleDecode (s : str) = (replicate (fromIntegral (fst s)) (snd s)) : rleDecode str
+rleDecode encoded = concat (map rlePair encoded)
+  where
+    rlePair (n, c) = replicate (fromIntegral n) c
 
 main :: IO ()
 main = do
   let code = "aaaabbaaa"
   print $ rleEncode code
   let encoded = rleEncode code
-  print $ snd ((rleEncode code) !! 0)
-  print $ decodeStep (encoded !! 0)
-  print $ rleDecode (rleEncode code)
+  print $ rleDecode encoded
