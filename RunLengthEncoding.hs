1d8c3bdcbe9f4d097f58e1a2d61113f8a081b054
ElectrixFox <lukecphillips2006@gmail.com>
2025-01-10 15:59:32 +0000

    RLE Decode
    Got the RLE decoding working.

--- RunLengthEncoding.hs
+++ RunLengthEncoding.hs
@@ -14,12 +14,21 @@ rleEncode :: String -> [(Integer, Char)]
 rleEncode inp = getCounts (group inp)
 -- rleEncode inp = [0, group (inp)]
 
+-- rleDecode
+decodeStep :: (Integer, Char) -> String
+decodeStep encoded = replicate (fromIntegral (fst encoded)) (snd encoded)
+
 -- rleDecode [(4,'a'), (2, 'b'), (3, 'a')] -> "aaaabbaaa"
 rleDecode :: [(Integer, Char)] -> String
-rleDecode inp = replicate inp
+rleDecode [] = " " 
+rleDecode encoded = concat (map (decodeStep) encoded)
+-- rleDecode (s : str) = (replicate (fromIntegral (fst s)) (snd s)) : rleDecode str
 
 main :: IO ()
 main = do
   let code = "aaaabbaaa"
   print $ rleEncode code
+  let encoded = rleEncode code
+  print $ snd ((rleEncode code) !! 0)
+  print $ decodeStep (encoded !! 0)
   print $ rleDecode (rleEncode code)
